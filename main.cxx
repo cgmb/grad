b425232ac24f9f4a859dffa6657f28f1bb232175
Cory Bloor <cgbloor@ucalgary.ca>
2017-10-14 20:03:50 -0600

    Specify OpenGL core profile

--- main.cxx
+++ main.cxx
@@ -298,6 +298,7 @@ int main(int argc, char** argv) {
   }
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
